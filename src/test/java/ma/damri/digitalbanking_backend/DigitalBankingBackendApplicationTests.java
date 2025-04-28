699f43cb124a22710305271818d63a319cf1e0ce
d-sar <damrisara1@gmail.com>
2025-04-28 09:29:00 +0100

    first commit

---
+++ src/test/java/ma/damri/digitalbanking_backend/DigitalBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.damri.digitalbanking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
